62292ca498de9bb26ca2904ae8c2a2a181920ca0
Jess Rezac <jess.rezac@gmail.com>
2019-06-05 21:48:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.status = "complete"
+    if self.status == "complete"
       self.receiver.deposit(-@amount)
       self.sender.deposit(@amount)
       self.status = "reversed"

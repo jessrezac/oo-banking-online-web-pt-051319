00ad46752b63dcc328518d7d59a10fb167e41365
Jess Rezac <jess.rezac@gmail.com>
2019-06-05 21:14:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    #   can check that both accounts are valid
+    #   calls on the sender and receiver's #valid? methods
+  end
+
+  def execute_transaction
+    #   can execute a successful transaction between two accounts
+    #   each transfer can only happen once
+    #   rejects a transfer if the sender doesn't have a valid account
+  end
+
+  def reverse_transfer
+    #   can reverse a transfer between two accounts
+    #   it can only reverse executed transfers
+  end
 end

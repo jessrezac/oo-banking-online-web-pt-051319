c72f56f50e4039f060cd0f818d80b127f840f3ec
Jess Rezac <jess.rezac@gmail.com>
2019-06-05 21:16:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  def intialize(name)
+  def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"

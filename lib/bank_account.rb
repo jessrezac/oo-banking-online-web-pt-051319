4b2131377d9134f2ff2d7ece7711f8ddf82cfda4
Jess Rezac <jess.rezac@gmail.com>
2019-06-05 21:10:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  def intialize
+    # can initialize a Bank Account (FAILED - 1)
+    # initializes with a name (FAILED - 2)
+    # always initializes with balance of 1000 (FAILED - 3)
+    # always initializes with a status of 'open' (FAILED - 4)
+    # can't change its name
 
+  def deposit
+    # can deposit money into its account (FAILED - 5)
+  end
+
+  def display_balance
+    # can display its balance (FAILED - 6)
+  end
+
+  def valid?
+    # is valid with an open status and a balance greater than 0 (FAILED - 7)
+  end
+
+  def close_account
+    # can close its account
+  end
 end
